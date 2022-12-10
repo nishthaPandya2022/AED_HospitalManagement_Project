c3a9b3a1e4a572404c21fae80848a897e6220c61
Shubhda09 <114841052+Shubhda09@users.noreply.github.com>
2022-12-10 17:25:11 -0500

    Add View Lab results and Lab inventory
    First commit for these will modify these java files

--- HealthCManagement/src/Laboratory/LabInventory.java
+++ HealthCManagement/src/Laboratory/LabInventory.java
@@ -5,10 +5,35 @@
  */
 package Laboratory;
 
+import org.sqlite.SQLiteDataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author shubhdashail
  */
 public class LabInventory {
-    
+      static SQLiteDataSource ds = null;
+        static Connection sqliteConnection;
+    public static Connection connectiondb(){
+        try{
+            ds = new SQLiteDataSource();
+            System.out.println("ds");
+            ds.setUrl("jdbc:sqlite:hospManagement.db");
+            System.out.println("ds set");
+            sqliteConnection = ds.getConnection();
+            System.out.println("sqliteConnection set");
+            //Class.forName("org.sqlite.JDBC");
+            //Connection conn = DriverManager.getConnection("jdbc;sqlite:laboratory.sqlite");
+            //JOptionPane.showMessageDialog(null, "successfully connected to "+conn);
+            //return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
 }
