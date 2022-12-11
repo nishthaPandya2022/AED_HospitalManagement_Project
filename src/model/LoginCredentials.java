0acdb8cc47c32d4546ac24fe536c3c9b5af6ba77
Nishtha Pandya <114362583+nishthaPandya2022@users.noreply.github.com>
2022-12-11 11:34:30 -0500

    ui changes
    models and ui added and updated
    
    rebasing changes from develop to this

---
+++ src/model/LoginCredentials.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author nishthapandya
+ */
+public class LoginCredentials {
+    
+    String loginID;
+    String userID;
+    String username;
+    String password;
+
+    public String getLoginID() {
+        return loginID;
+    }
+
+    public void setLoginID(String loginID) {
+        this.loginID = loginID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
